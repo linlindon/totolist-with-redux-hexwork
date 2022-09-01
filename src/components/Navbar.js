082830a80718f3df7290f2b32aba8f781a0b4c3b
Lin Yiling <ling@LindeMacBook-Pro.local>
2022-09-01 14:12:35 +0800

    delet unuse file

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav>
+        <h1><a href="#">ONLINE TODO LIST</a></h1>
+        <ul>
+            <li class="todo_sm"><a href="#"><span>王小明的代辦</span></a></li>
+            <li><a href="#loginPage">登出</a></li>
+        </ul>
+    </nav>     
+    )
+}
+export default Navbar
